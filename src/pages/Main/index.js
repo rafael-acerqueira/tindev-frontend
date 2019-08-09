6dd3d43a8b26814c8e5df0918d0fde796035097f
Rafael <rafael.acerqueirah@gmail.com>
2019-08-09 15:15:03 -0300

    Fetch devs from API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../assets/logo.svg'
 import like from '../../assets/like.svg'
 import dislike from '../../assets/dislike.svg'
@@ -14,84 +14,46 @@ import {
 	Footer
 } from './styles'
 
-const Main = ({ match }) => (
-	<Container>
-		<img src={logo} alt="logo" />
-		<List>
-			<ListItem>
-				<ImageList
-					src="https://avatars2.githubusercontent.com/u/9022148?v=4"
-					alt=""
-				/>
-				<Footer>
-					<Name>Nome do usuário</Name>
-					<Bio>Descrição</Bio>
-				</Footer>
-				<ButtonsContainer>
-					<Button type="button">
-						<ImageList src={dislike} alt="dislike" />
-					</Button>
-					<Button type="button">
-						<ImageList src={like} alt="like" />
-					</Button>
-				</ButtonsContainer>
-			</ListItem>
-			<ListItem>
-				<ImageList
-					src="https://avatars2.githubusercontent.com/u/9022148?v=4"
-					alt=""
-				/>
-				<Footer>
-					<Name>Nome do usuário</Name>
-					<Bio>Descrição</Bio>
-				</Footer>
-				<ButtonsContainer>
-					<Button type="button">
-						<ImageList src={dislike} alt="dislike" />
-					</Button>
-					<Button type="button">
-						<ImageList src={like} alt="like" />
-					</Button>
-				</ButtonsContainer>
-			</ListItem>
-			<ListItem>
-				<ImageList
-					src="https://avatars2.githubusercontent.com/u/9022148?v=4"
-					alt=""
-				/>
-				<Footer>
-					<Name>Nome do usuário</Name>
-					<Bio>Descrição</Bio>
-				</Footer>
-				<ButtonsContainer>
-					<Button type="button">
-						<ImageList src={dislike} alt="dislike" />
-					</Button>
-					<Button type="button">
-						<ImageList src={like} alt="like" />
-					</Button>
-				</ButtonsContainer>
-			</ListItem>
-			<ListItem>
-				<ImageList
-					src="https://avatars2.githubusercontent.com/u/9022148?v=4"
-					alt=""
-				/>
-				<Footer>
-					<Name>Nome do usuário</Name>
-					<Bio>Descrição</Bio>
-				</Footer>
-				<ButtonsContainer>
-					<Button type="button">
-						<ImageList src={dislike} alt="dislike" />
-					</Button>
-					<Button type="button">
-						<ImageList src={like} alt="like" />
-					</Button>
-				</ButtonsContainer>
-			</ListItem>
-		</List>
-	</Container>
-)
+import api from '../../services/api'
+
+const Main = ({ match }) => {
+	const [devs, setDevs] = useState([])
+
+	useEffect(() => {
+		const loadusers = async () => {
+			const { data } = await api.get('/devs', {
+				headers: { user: match.params.id }
+			})
+
+			setDevs(data)
+		}
+		loadusers()
+	}, [match.params.id])
+
+	return (
+		<Container>
+			<img src={logo} alt="logo" />
+			<List>
+				{devs.map(dev => (
+					<ListItem key={dev._id}>
+						<ImageList src={dev.avatar} alt={dev.name} />
+						<Footer>
+							<Name>{dev.name}</Name>
+							<Bio>{dev.bio}</Bio>
+						</Footer>
+						<ButtonsContainer>
+							<Button type="button">
+								<ImageList src={dislike} alt="dislike" />
+							</Button>
+							<Button type="button">
+								<ImageList src={like} alt="like" />
+							</Button>
+						</ButtonsContainer>
+					</ListItem>
+				))}
+			</List>
+		</Container>
+	)
+}
 
 export default Main
