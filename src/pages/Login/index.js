1519a57e2cd8f8e22d4d22accb9da2ffd6c2d40b
Rafael <rafael.acerqueirah@gmail.com>
2019-08-09 13:15:17 -0300

    Add service to API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/logo.svg'
 import { Container, Form, Input, Button } from './styles'
 
-const Login = () => (
-	<Container>
-		<Form>
-			<img src={logo} alt="logo" />
-			<Input placeholder="Digite seu usuário no Github" />
-			<Button type="submit">Enviar</Button>
-		</Form>
-	</Container>
-)
+import api from '../../services/api'
+
+const Login = ({ history }) => {
+	const [username, setUsername] = useState('')
+
+	const handleSubmit = async e => {
+		e.preventDefault()
+		const { data } = await api.post('/devs', { username })
+
+		const { _id } = data
+
+		history.push(`/dev/${_id}`)
+	}
+
+	return (
+		<Container>
+			<Form>
+				<img src={logo} alt="logo" />
+				<Input
+					placeholder="Digite seu usuário no Github"
+					value={username}
+					onChange={e => setUsername(e.target.value)}
+				/>
+				<Button type="submit" onClick={handleSubmit}>
+					Enviar
+				</Button>
+			</Form>
+		</Container>
+	)
+}
 
 export default Login
